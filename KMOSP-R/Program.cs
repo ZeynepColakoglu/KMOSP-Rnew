1393e9b833704d9233a9bc4e4f56da25af3d6d90
mfdsoftdev <mfdsoftdev@gmail.com>
2023-04-01 11:20:43 +0300

    feat: add question pattern

--- KMOSP-R/Program.cs
+++ KMOSP-R/Program.cs
@@ -66,6 +66,100 @@
             gizleA = false; gizleB = false;
             gizleC = false; gizleD = false;
 
+        soruNoktasi:
+            Console.Clear();
+            Console.WriteLine(soruNo + "-)" + soru);
+            Console.WriteLine("A) " + (!gizleA ? a : ""));
+            Console.WriteLine("B) " + (!gizleB ? b : ""));
+            Console.WriteLine("C) " + (!gizleC ? c : ""));
+            Console.WriteLine("D) " + (!gizleD ? d : ""));
+
+        yanitNoktasi:
+            bool jokerHak = seyirciHak || yuzdeHak || telefonHak;
+
+            Console.Write("Cevabınızı Giriniz veya");
+            if (jokerHak) Console.Write(" Joker için J'ye,");
+            Console.Write(" Çekilmek için R'ye Basınız: ");
+            char secim = char.Parse(Console.ReadLine().ToUpper());
+
+            if (secim == 'J')
+            {
+                if (!jokerHak)
+                {
+                    Console.WriteLine("Joker Hakkınız Bitmiştir..");
+                    goto yanitNoktasi;
+                }
+
+                Console.WriteLine("1-) " + (seyirciHak ? "Seyirci" : ""));
+                Console.WriteLine("2-) " + (yuzdeHak ? "%50" : ""));
+                Console.WriteLine("3-) " + (telefonHak ? "Telefon" : ""));
+                Console.Write("Seçiminizi Yapınız: ");
+                int jokerCevap = int.Parse(Console.ReadLine());
+
+                if (jokerCevap == 1 && seyirciHak)
+                {
+                    Console.WriteLine("A) %" + seyirciYuzdeA);
+                    Console.WriteLine("B) %" + seyirciYuzdeB);
+                    Console.WriteLine("C) %" + seyirciYuzdeC);
+                    Console.WriteLine("D) %" + seyirciYuzdeD);
+                    seyirciHak = false;
+                }
+                else if (jokerCevap == 2 && yuzdeHak)
+                {
+                    // TODO: Bu kısım sorudan bağımsız kullanılabilmeli.
+                    gizleB = true;
+                    gizleC = true;
+
+                    yuzdeHak = false;
+                    goto soruNoktasi;
+                }
+                else if (jokerCevap == 3 && telefonHak)
+                {
+                    Console.WriteLine("1-) " + telefonJoker1);
+                    Console.WriteLine("2-) " + telefonJoker2);
+                    Console.WriteLine("3-) " + telefonJoker3);
+                    Console.Write("Kimi Aramak İstersiniz? : ");
+                    int telefonCevap = int.Parse(Console.ReadLine());
+
+                    if (soruNo <= 7 || telefonCevap == 3) Console.WriteLine("Cevap Kesinlikle " + cevap);
+                    else if (telefonCevap == 1)// TODO: Şıklar rastgele olacak. 1Doğru 1Yanlış cevap şeklinde
+                        Console.WriteLine("Emin Değilim. Ancak A veya B olduğunu düşünüyorum");
+                    else if (telefonCevap == 2)
+                        Console.WriteLine("Bilemedim. Kusura bakma");
+                    telefonHak = false;
+                }
+                else
+                    Console.WriteLine("Bu Joker Daha Önce Kullanıldı..");
+
+                goto yanitNoktasi;
+            }
+            else if (secim == 'R')
+            {
+                Console.WriteLine(kasa + " TL Kazanacaksınız.");
+                Console.Write("Çekilmek İstediğinize Emin Misiniz? : ");
+                char cekilCevap = char.Parse(Console.ReadLine());
+
+                if (cekilCevap == 'E')
+                {
+                    Console.WriteLine("Tebrikler, " + kasa + " TL Kazandınız.");
+                    Console.WriteLine("Oyun Bitti");
+                    //return;
+                    Environment.Exit(0);
+                }
+                goto soruNoktasi;
+            }
+            else if (secim != cevap)
+            {
+                Console.WriteLine("Elendiniz, Kazandığınız Tutar: " + baraj + " TL");
+                Console.WriteLine("Oyun Bitti");
+                //return;
+                Environment.Exit(0);
+            }
+
+            kasa = odul;
+            Console.WriteLine("Tebrikler, Kazandığınız Tutar: " + kasa + " TL");
+            Console.WriteLine("\nSonraki Soruya Geçmek  İçin Enter'a Basın..");
+            Console.ReadLine();
         }
     }
 }

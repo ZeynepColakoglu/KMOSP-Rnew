0fb00ea106f91604d54f295c914211a1cea002d0
mfdsoftdev <mfdsoftdev@gmail.com>
2023-04-01 11:16:12 +0300

    feat: add variables

--- KMOSP-R/Program.cs
+++ KMOSP-R/Program.cs
@@ -40,6 +40,13 @@
                 Console.ReadLine();
             }
 
+            bool seyirciHak = true, yuzdeHak = true, telefonHak = true;
+            int kasa = 0;
+            int soruNo = 1, odul, baraj;
+            int seyirciYuzdeA, seyirciYuzdeB, seyirciYuzdeC, seyirciYuzdeD;
+            bool gizleA, gizleB, gizleC, gizleD;
+            string soru, a, b, c, d;
+            char cevap;
 
         }
     }

4a3462c26d61a4babc969ef5c6ee46ac0fb26e0d
mfdsoftdev <mfdsoftdev@gmail.com>
2023-04-01 11:10:24 +0300

    feat: get user info

--- KMOSP-R/Program.cs
+++ KMOSP-R/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Kim Milyoner Olmak İster\n");
+
+            Console.Write("İsim Giriniz: ");
+            string isim = Console.ReadLine();
+            Console.Write("Soyisim Giriniz: ");
+            string soyisim = Console.ReadLine();
         }
     }
 }
